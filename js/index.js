1deebbf22e9885c773d909e0d7410837e361362c
Justin White <just6979@gmail.com>
2012-06-13 04:28:37 +0000

    Moved tiny index.js to inline in <head>.~

--- js/index.js
+++
@@ -1,7 +0,0 @@
-// will contain global site JS stuff
-
-$(document).ready(function(){
-    $("section header").click(function(event){
-        $(this).siblings('.content').toggleClass("visible invisible");
-    });
-});
